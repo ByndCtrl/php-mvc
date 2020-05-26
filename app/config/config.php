8f6374e056d78e8e406b17e50136414b8ea2dbd3
Jure Lucian Boban <jlboban17@gmail.com>
2020-05-26 19:11:03 +0200

    Folder structure setup
    Configured .htaccess files.
    
    Added basic view templates for testing.

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'user');
+    define('DB_PASS', 'pass');
+    define('DB_NAME', 'dbname');
+
+    define('APPROOT', dirname(__FILE__, 2));
+
+    define('URLROOT', 'http://localhost/php-mvc');
+
+    define('SITENAME', 'PHP-MVC');
---
+++ app/init.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'config/config.php';
+
+mb_internal_encoding("UTF-8");
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
