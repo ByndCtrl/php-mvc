5b83f9f04705a5d839285697450939c48e3c4839
Jure Lucian Boban <jlboban17@gmail.com>
2020-05-31 18:17:39 +0200

    Changed folder structure.
    Reupload.
    
    Added namespaces, docs and improvements to several scripts.

---
+++ App/Credentials.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Credentials
+{
+    const DB_HOST = 'database-host';
+    const DB_NAME = 'database-name';
+    const DB_USER = 'database-user';
+    const DB_PASSWORD = 'database-password';
+}
